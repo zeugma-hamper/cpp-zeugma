89885993294f950f54c8fee119cc7b56bcdfe8a4
Blake Tregre <blake.tregre@gmail.com>
2020-09-17 09:46:19 -0700

    meson.build: add shader compilation custom command

--- compile_shader.sh
+++ compile_shader.sh
@@ -5,4 +5,6 @@
 # $3 varying file
 # $4 output filename
 
-bld/shaderc -i bgfx/src -f $2 -o $4 --type $1 --varyingdef $3 --profile 460
+echo 'YO! add shaders to meson.build now, please'
+echo 'would have run:'
+echo bld/shaderc -i bgfx/src -f $2 -o $4 --type $1 --varyingdef $3 --profile 460
