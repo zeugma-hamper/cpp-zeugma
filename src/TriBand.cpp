2daade3b8882814496411595e2ef42cf4373044a
Blake Tregre <blake.tregre@gmail.com>
2020-10-15 10:52:23 -0700

    switch to 3 collages per wall

--- src/TriBand.cpp
+++ src/TriBand.cpp
@@ -16,43 +16,30 @@ TriBand::TriBand (f64 _width, f64 _height, std::vector<FilmInfo> const &_films)
   f64 const band_height = _height / 3.0;
   f64 const band_width = _width;
 
-  f64 const size_factor = 0.225;
-  f64 const placement_factor = 0.125;
+  f64 const size_factor = 0.3;
+  f64 const placement_factor = 1.0 / 3.0;
 
   Vect const xxx = Vect (1.0, 0.0, 0.0);
   Vect const yyy = Vect (0.0, 1.0, 0.0);
 
-  //top band
   Vect const collage_center = band_height * yyy;
-  Collage *collage = new Collage (5, _films,
-                                  size_factor * band_width,
-                                  size_factor * band_height);
-  collage->Translate(collage_center
-                     - 3.0 * placement_factor * xxx * band_width);
-  AppendChild(collage);
 
-  Collage *collage2 = new Collage (5, _films,
-                                   size_factor * band_width,
-                                   size_factor * band_height);
-  collage2->Translate(collage_center
-                      - placement_factor * xxx * band_width);
-  AppendChild(collage2);
+  std::array<Vect, 3> const positions
+    = {collage_center - 1.0 * placement_factor * xxx * band_width,
+       collage_center,
+       collage_center + 1.0 * placement_factor * xxx * band_width};
 
-  Collage *collage3 = new Collage (5, _films,
-                                  size_factor * band_width,
-                                  size_factor * band_height);
-  collage3->Translate(collage_center
-                      + placement_factor * xxx * band_width);
-  AppendChild(collage3);
+  //top band - 4 collages
+  for (Vect const &v : positions)
+    {
+      Collage *collage = new Collage (5, _films,
+                                      size_factor * band_width,
+                                      size_factor * band_height);
+      collage->Translate(v);
+      AppendChild(collage);
+    }
 
-  Collage *collage4 = new Collage (5, _films,
-                                   size_factor * band_width,
-                                   size_factor * band_height);
-  collage4->Translate(collage_center
-                      + 3.0 *  placement_factor * xxx * band_width);
-  AppendChild(collage4);
-
-  // middle band
+  // middle band - 10 escaped, currently non-mobile, elements
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<>  film_distrib (0, _films.size ()-1);
